cde05c498b369e21d46f412d2b2304ce9aa9c572
Leandro Luna <leandro.j.luna@gmail.com>
2022-08-11 13:02:19 -0300

    ajuste path initial

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -45,7 +45,7 @@ export const routes: Routes = [
       },
     ],
   },
-  { path: '', redirectTo: 'pages', pathMatch: 'full' },
+  { path: '', redirectTo: 'pages/dashboardExp', pathMatch: 'full' },
   { path: '**', redirectTo: 'pages' },
 ];
 
